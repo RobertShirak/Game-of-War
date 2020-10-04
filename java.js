41bd108dbebf600b2a5a3f8a28125793f7acb62e
Robert Shirak <bcshirak1@gmail.com>
2020-10-04 18:52:32 -0400

    deck and shuffle

--- java.js
+++ java.js
@@ -18,3 +18,31 @@ class Player {
 
 // create Deck and shuffle
 
+class Deck {
+    constructor () {
+        this.suit = ["Diamonds","Hearts", "Clubs", "Spades"]
+        this.rank = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
+        this.value = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+        this.cards = []
+        this.length = 52
+        for (let i = 0; i < this.suit.length; i++) {
+            for (let j = 0; j < this.rank.length; j++) {
+                let card = new Card(this.suit[i], this.rank[j], this.value[j])
+                this.cards.push(card)
+            }
+        }   
+    }
+    //// fisher-yates shuffle 
+    shuffle = () => {
+        for (let i = this.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            let shuffled = this.cards[i];
+            this.cards[i] = this.cards[j];
+            this.cards[j] = shuffled;
+        }    
+    }
+}
+
+
+// create game class and logic to make the game work
+
