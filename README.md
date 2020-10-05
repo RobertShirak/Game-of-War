a94cce535cabc02e029fac2eaa082450527ebce7
Robert Shirak <bcshirak1@gmail.com>
2020-10-04 21:06:52 -0400

    fineto and readme fineto

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # Game-of-War
+
+# Introduction
+Project 2. Game of War.
+
+# Rules for War
+THE DEAL:
+
+The deck is divided evenly, with each player receiving 26 cards, dealt one at a time, face down. Anyone may deal first. Each player places their stack of cards face down, in front of them.
+
+THE PLAY:
+
+Each player turns up a card at the same time and the player with the higher card takes both cards and puts them, face down, on the bottom of his stack.
+
+If the cards are the same rank, it is War. Each player turns up 3 cards face down and one card face up. The player with the higher cards takes both piles. If the turned-up cards are again the same rank, each player places another 3 cards down and turns another card face up. The player with the higher card takes the pile, and so on.
+
+HOW TO KEEP SCORE:
+
+The game ends when one player has won all 52 cards or does not have enough cards to play war.
+
+# How to Play
+Within the index.html you can locate the console and the game will be complete with the winner being displayed. My favorite way to access the game is copying the javascript from the source tab within developer tools and using Repl.it to run the code over and over to see who wins each time and how many rounds it takes.
+
+# Helpful Sources
+Fisher-Yates Shuffle: https://medium.com/@oldwestaction/randomness-is-hard-e085decbcbb2
+
+Bicycle Cards: https://bicyclecards.com/how-to-play/war/
+
+Repl.it
+
+# Developed By
+Robert Shirak Jr
+
+bcshirak1@gmail.com
--- java.js
+++ java.js
@@ -46,3 +46,110 @@ class Deck {
 
 // create game class and logic to make the game work
 
+// deal
+// start the first round (start so they can keep track of rounds)
+// played cards
+/// keep track of played cards
+/// collect cards
+/// compare cards
+/// check if someone has 0 cards or not // if someone does not have enough cards for war the opposite player wins
+/// create war stipulation 
+
+
+
+
+
+class Game { 
+    constructor() {
+        this.round = 0
+        this.warCards = []
+        this.twoPlayers = [new Player ("Player 1"), new Player ("Player 2")]
+        this.deck = new Deck ()
+        this.deck.shuffle();
+    } 
+    dealCards() {    
+        for (let i = 0; i < this.deck.length; i++) {
+            if (i % 2 === 0) {
+                this.twoPlayers[0].hand.unshift(this.deck.cards[i])
+            } else {
+                this.twoPlayers[1].hand.unshift(this.deck.cards[i])
+            }
+        }
+        this.deck = []
+        this.startRound()
+    }
+    startRound() {
+        this.round += 1;
+        this.playedCards();
+    }
+    playedCards() {
+        this.twoPlayers[0].cardInPlay.unshift(this.twoPlayers[0].hand[0]);
+        this.twoPlayers[0].hand.shift();
+        this.twoPlayers[1].cardInPlay.unshift(this.twoPlayers[1].hand[0]);
+        this.twoPlayers[1].hand.shift();
+        console.log(`${this.twoPlayers[0].name} Played: ${this.twoPlayers[0].cardInPlay[0].rank} of ${this.twoPlayers[0].cardInPlay[0].suit} | ${this.twoPlayers[1].name} Played: ${this.twoPlayers[1].cardInPlay[0].rank} of ${this.twoPlayers[1].cardInPlay[0].suit}`)
+        this.compareCards();
+    }
+    collectCards(r,s) {
+        this.twoPlayers[r].hand.push(...this.warCards);
+        this.twoPlayers[r].hand.push(...this.twoPlayers[r].cardInPlay, ...this.twoPlayers[s].cardInPlay);
+        this.twoPlayers[r].cardInPlay = []
+        this.twoPlayers[s].cardInPlay = []
+        this.warCards = [];
+        console.log(`${this.twoPlayers[r].name} Wins Round ${this.round}! |   ${this.twoPlayers[r].name} Has: ${this.twoPlayers[r].hand.length} Cards  |   ${this.twoPlayers[s].name} Has: ${this.twoPlayers[s].hand.length} Cards`)
+        this.checkGame()
+    }
+    compareCards() {
+        if (this.twoPlayers[0].cardInPlay[0].value > this.twoPlayers[1].cardInPlay[0].value) {
+            this.collectCards(0,1);
+        } else if (this.twoPlayers[0].cardInPlay[0].value < this.twoPlayers[1].cardInPlay[0].value) {
+            this.collectCards(1,0)
+        } else {
+            if (this.twoPlayers[0].hand.length === 0) {
+                this.winnerMessage(this.twoPlayers[1].name)
+            } else if (this.twoPlayers[1].hand.length === 0) {
+                this.winnerMessage(this.twoPlayers[0].name)
+            } else {
+            this.startWar();
+        }
+    }
+}
+    
+    checkGame() {
+        if (this.twoPlayers[0].hand.length === 0) {
+            this.winnerMessage(this.twoPlayers[1].name)
+        } else if (this.twoPlayers[1].hand.length === 0) {
+            this.winnerMessage(this.twoPlayers[0].name)
+        } else {
+            this.startRound();
+        }
+    }
+
+    winnerMessage(winner) {
+        console.log(`CONGRATS! ${winner} YOU WON! in ${this.round} Rounds!`)
+    }
+
+    startWar() {
+            for (let i = 0; i < 3; i++) {
+                if (i >= this.twoPlayers[0].hand.length - 1){
+                } else{
+                    this.warCards.unshift(this.twoPlayers[0].hand[0]);
+                    this.twoPlayers[0].hand.shift()
+                }
+            }   
+            for (let i = 0; i < 3; i++) {
+                if (i >= this.twoPlayers[1].hand.length - 1){
+                } else{
+                    this.warCards.unshift(this.twoPlayers[1].hand[0]);
+                    this.twoPlayers[1].hand.shift()
+                }
+            }
+            console.log("WAR!");
+            this.playedCards();
+        }
+}
+
+
+/// call on game class for new game with the help of deal cards function
+let newGame = new Game()
+newGame.dealCards();
